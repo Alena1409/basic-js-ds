4f4f5054196a616c719bef6dab1d7788b5a00ef4
Alena1409 <a77993519@gmail.com>
2025-01-03 15:06:25 +0400

    feat: add class Queue

--- src/queue.js
+++ src/queue.js
@@ -13,22 +13,39 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor () {
+    this.head = null;
+    this.tail = null;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    const newNode = new Node(value);
+    if(!this.head || !this.tail) {
+      this.head = newNode;
+      this.tail = newNode;
+    }
+    this.tail.next = newNode;
+    this.tail = newNode;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    if(!this.head) return null;
+    const value = this.head.value;
+    const newHead = this.head.next;
+    this.head = newHead;
+    return value;
+ }
 }
 
 module.exports = {
